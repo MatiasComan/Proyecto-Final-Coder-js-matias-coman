8a58a4ac1a8f2fba6bd5de9a6560750b1431e83e
MatiasComan <103226168+MatiasComan@users.noreply.github.com>
2023-03-19 20:46:03 -0300

    Subida actualización de carrito nuevo

---
+++ js/home.js
@@ -0,0 +1,21 @@
+const pintarProductos = (data) => {
+    const contenedor = document.getElementById("producto-contenedor");
+
+    data.forEach(producto => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML += `<div class="card-image">
+                          <img src=${producto.imagen}>
+                          <span class="card-title">${producto.nombre}</span>
+                          <br>
+                          <br>
+                          <button <a class="btn-floating halfway-fab wabes-effect waves-light red"><i id=${producto.id} class="material-icons agregar">Agregar!</i></a></button>
+                        </div>
+                        <div class="card-content">
+                            <p> ${producto.desc}</p>
+                            <p>$ ${producto.precio}</p>
+                        </div>
+                       `
+      contenedor.appendChild(div);
+    });
+  };
